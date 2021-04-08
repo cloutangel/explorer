f09f2f28384c8e86c7085bd34ea18343136711e7
Don Hardman <don.hardman@muvon.io>
2021-04-08 21:26:15 +0700

    Add Cloutangel API lib to request data

---
+++ app/src/lib/Cloutangel.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Lib;
+
+use Muvon\KISS\RequestTrait;
+
+final class Cloutangel {
+  use RequestTrait;
+
+  protected string $network;
+  final protected function __construct(protected string $url, protected string $token) {}
+
+  public static function create(string $url, string $token) {
+    $Obj = new self($url, $token);
+    $Obj->request_type = 'json';
+    return $Obj;
+  }
+
+  public function setNetwork(string $network) {
+    $this->network = $network;
+  }
+
+  public function getBlockCount() {
+    return $this->call('block/height');
+  }
+
+  public function getBlock(int|string $height) {
+    return $this->call('block/get/' . $height);
+  }
+
+  public function getBlockList(array $heights) {
+    return $this->call('block/list', ['heights' => $heights], 'POST');
+  }
+
+  public function getTx(string $hash) {
+    return $this->call('tx/get/' . $hash);
+  }
+
+  public function getTxList(array $hashes) {
+    return $this->call('tx/list', ['hashes' => $hashes], 'POST');
+  }
+
+  public function getAddressBalance(string $address) {
+    return $this->call('address/balance/' . $address);
+  }
+
+  public function getAddressInfo(string $address, int $offset = 0, int $limit = 100) {
+    return $this->call('address/get/' . $address, ['offset' => $offset, 'limit' => $limit]);
+  }
+
+  protected function call(string $method, array $params = [], $http_method = 'GET') {
+    [$err, $response] = $this->request(
+      $this->url . '/v1/' . $method,
+      $params,
+      $http_method,
+      [
+        'X-API-Token: ' . $this->token
+      ]
+    );
+
+    if ($err) {
+      return [$err, null];
+    }
+
+    return $response;
+  }
+}
